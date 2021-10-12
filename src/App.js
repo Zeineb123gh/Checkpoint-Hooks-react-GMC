58b03839ec2483315479a1229881fc87500ddf7f
Zeineb123gh <zeineb.ghandri@gmail.com>
2021-10-13 00:13:54 +0200

    movie app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+// CSS
+import "./App.css";
+
+// data
+import { moviesData } from "./constants/data";
+
+// components
+import Footer from "./Component/Footer/Footer";
+import MovieList from "./Component/MovieList/MovieList";
+import Navbar from "./Component/Navbar/Navbar";
+import AddMovie from "./Component/AddMovie/AddMovie";
 
 function App() {
+  const [movies, setMovies] = useState(moviesData);
+  const [searchTitle, setSearchTitle] = useState("");
+  const [searchRating, setSearchRating] = useState(0);
+
+  const handleMovie = (movie) => {
+    setMovies([...movies, movie]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar
+        setSearchTitle={setSearchTitle}
+        setSearchRating={setSearchRating}
+        searchRating={searchRating}
+      />
+
+      <div
+        style={{ display: "flex", alignContent: "flex-start", marginTop: "5%" }}
+      >
+        <AddMovie handleMovie={handleMovie} />
+        <MovieList
+          movies={movies}
+          searchTitle={searchTitle}
+          searchRating={searchRating}
+        />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/constants/data.js
@@ -0,0 +1,45 @@
+export const moviesData = [
+  {
+    id: Math.random(),
+    image: "https://fr.web.img6.acsta.net/pictures/16/12/09/09/51/046535.jpg",
+    rating: 4,
+    name: "SPLIT",
+    date: "2017",
+    type: "Action",
+    description:
+      "Kevin a déjà révélé 23 personnalités, avec des attributs physiques différents pour chacune, à sa psychiatre dévouée, la docteure Fletcher, mais l’une d’elles reste enfouie au plus profond de lui...",
+  },
+  {
+    id: Math.random(),
+    image:
+      "https://fr.web.img6.acsta.net/c_310_420/pictures/17/01/25/09/36/363964.jpg",
+    rating: 5,
+    name: "Lion",
+    date: "2017",
+    type: "Drama",
+    description:
+      "Une incroyable histoire vraie : à 5 ans, Saroo se retrouve seul dans un train traversant l’Inde qui l’emmène malgré lui à des milliers de kilomètres de sa famille. Perdu, le petit garçon doit apprendre à survivre seul dans l’immense ville de Calcutta. Après des mois d’errance, il est recueilli dans un orphelinat et adopté par un couple d’Australiens",
+  },
+  {
+    id: Math.random(),
+    image:
+      "https://media.senscritique.com/media/000020011667/325/The_Nightingale.jpg",
+    rating: 4,
+    name: "The Nightengale",
+    date: "2021",
+    type: "Action",
+    description:
+      "A mentally unstable veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action by attempting to liberate a presidential campaign worker and an underage prostitute.",
+  },
+  {
+    id: Math.random(),
+    image:
+      "https://pictures.artify.tn/media/cr5z0acwrsahhs5ttiqv.jpg?width=300",
+    rating: 4,
+    name: "زهرة حلب",
+    date: "2020",
+    type: "Drama",
+    description:
+      "In a self-destructing world, a vengeful Australian policeman sets out to stop a violent motorcycle gang.",
+  },
+];
